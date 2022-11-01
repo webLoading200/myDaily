eea5fb77231d94276c4ebb3228148cef69587a73
赖伟 <1217299814@qq.com>
2022-11-01 18:17:08 +0800

    feat：新增响应式优化

--- vue2响应式原理简单实现/myVue.js
+++ vue2响应式原理简单实现/myVue.js
@@ -47,18 +47,16 @@ function defineReactive(data, key, val) {
 		configurable: true,
 		get: function() {
 			// 读取 data[key] 时触发
-			console.log('getter', this);
 			dep.depend(key);
 			return val;
 		},
 		set: function(newVal) {
 			// 修改 data[key] 时触发
-			console.log('setter', newVal);
 			if (val === newVal) {
 				return;
 			}
 			val = newVal;
-			dep.notify(newVal);
+			dep.notify(key, newVal);
 		}
 	})
 	if (typeof val == 'object') {
@@ -75,13 +73,22 @@ class Dep {
 		this.subs = [];
 	}
 	addSub(sub) {
-		console.log(window.target)
-		for (let i = 0; i < this.subs.length; i++) {
-			if (this.subs[i] == window.target) {
-				this.subs.splice(i, 1);
+		if (window.target) {
+			console.log(window.temp)
+			for (let i = 0; i < this.subs.length; i++) {
+				if (this.subs[i].target == window.target) {
+					this.subs.splice(i, 1);
+				}
 			}
+			let d = {
+				target: window.target,
+				temp: window.temp
+			}
+			this.subs.push(d);
+			window.target = null
+			window.temp = null
 		}
-		this.subs.push(window.target);
+
 	}
 	removeSub(sub) {
 		if (this.subs.length) {
@@ -92,39 +99,131 @@ class Dep {
 	depend(name) {
 		this.addSub(name);
 	}
-	notify(e) {
-		console.log(this.subs)
+	notify(key, e) {
+		console.log(this.subs, key, e)
 		const subs = this.subs.slice();
 		for (let i = 0; i < subs.length; i++) {
 			//触发更新函数。{{}} v-if v-model 等
-			console.log(subs[i] + "更新了")
-			subs[i].innerHTML = e
+			subs[i].target.innerHTML = render(subs[i].temp, vueData)
 		}
 	}
 }
-const people = {
-	name: 1,
-	age: 12,
-	parents: {
-		dad: 'a',
-		mom: 'b'
-	},
-	arr: ['d', 'e'],
-	list: ['f', 'e']
-};
-new Observer(people);
 
-let dom = document.createElement('div')
-window.target = dom
-dom.innerHTML = people.name;
-document.body.appendChild(dom)
-/**********************************************************************/
-let input = document.createElement('input')
-window.target = input
-input.value = people.name;
-input.type = "text"
-input.addEventListener('input', e => {
-	console.log(input.value)
-	people.name = input.value
-})
-document.body.appendChild(input)
+//下面的步骤类似于vue的模板编译后的虚拟dom创建真实dom并绑定数据。
+class VNnode {
+	//构造函数
+	//一个节点会有标签（tag）属性（props）value，标签类型
+	constructor(tag, props, value, type) {
+		//标签名转小写
+		this.tag = tag && tag.toLowerCase()
+		this.props = props
+		this.value = value
+		this.type = type
+		this.children = []
+	}
+	//追加子元素
+	appendChild(vnode) {
+		this.children.push(vnode)
+	}
+}
+//将html结构转为虚拟dom
+function getVNode(node) {
+	//获取节点类型
+	let nodeType = node.nodeType
+	//定义虚拟DOM
+	let _vnode = null
+	///nodeType 为一表示标签节点
+	if (nodeType === 1) {
+		let nodeName = node.nodeName
+		//节点属性
+		let attrs = node.attributes
+		//获取props
+		let _attrObj = {}
+		//遍历所有节点
+		for (let i = 0; i < attrs.length; i++) {
+			_attrObj[attrs[i].nodeName] = attrs[i].nodeValue
+		}
+		//nodeName标签名 ,_attrObj 属性 第三位value  nodeType 标签类
+		// <div></div>,标签节点,中间的value是文本节点,所有value是undefined
+		//生成虚拟DOM
+		_vnode = new VNnode(nodeName, _attrObj, undefined, nodeType)
+		//node的子节点
+		let childNodes = node.childNodes
+		// 打印验证生成的子节点
+		//console.log(childNodes)
+		//子节点进行循环遍历生成虚拟DOM
+		for (let i = 0; i < childNodes.length; i++) {
+			_vnode.appendChild(getVNode(childNodes[i]))
+		}
+	}
+	//文本节点
+	//<div>哈哈哈</div> 中间的哈哈哈才能表示文本节点,所以标签名和属性时undefined
+	else if (nodeType === 3) {
+		_vnode = new VNnode(undefined, undefined, node.nodeValue, nodeType)
+	}
+	return _vnode
+}
+
+const reg = /\{\{(\w+)\}\}/;
+
+function parseVNode(vnode) {
+	//获取类型
+	let type = vnode.type
+	//定义真实DOM
+	let _node = null
+	//文本节点
+	if (type === 3) {
+		if (vnode.value) {
+
+
+		}
+		return document.createTextNode(vnode.value) //创建文本节点
+
+	} else if (type === 1) {
+		//元素节点
+		_node = document.createElement(vnode.tag) // 创建元素标签名
+		//1.属性
+		let props = vnode.props //props此时为键值对 即还原 class = 'value'
+		Object.keys(props).forEach((key) => {
+			let attrName = key //属性名
+			let attrValue = props[key] //属性值
+			//绑定标签的属性值
+			_node.setAttribute(attrName, attrValue)
+		})
+		//2.子节点
+		let children = vnode.children
+		//遍历子节点 ,子节点此时为虚拟DOM
+		children.forEach((subvnode) => {
+			if (reg.test(subvnode.value)) {
+				window.target = _node
+				window.temp = subvnode.value
+				subvnode.value = render(subvnode.value, vueData)
+			}
+			_node.appendChild(parseVNode(subvnode)) //调用转换真实DOM函数,递归转换为子元素
+		})
+		return _node
+	}
+}
+//解析vue模板{{}}语法
+function render(template, data) {
+	const reg = /\{\{(\w+)\}\}/; // 模板字符串正则
+	if (reg.test(template)) { // 判断模板里是否有模板字符串
+		const name = reg.exec(template)[1]; // 查找当前模板里第一个模板字符串的字段
+		template = template.replace(reg, data[name]); // 将第一个模板字符串渲染
+		return render(template, data); // 递归的渲染并返回渲染后的结构
+	}
+	return template; // 如果模板没有模板字符串直接返回
+}
+let vueData = {}
+function myVue(ele,data) {
+	vueData = data
+	new Observer(vueData);
+	let root = ele
+	//转换为虚拟DOM
+	let vroot = getVNode(root)
+	let Edom = parseVNode(vroot)
+	document.body.removeChild(root) //移除原来的dom
+	document.body.appendChild(Edom) //渲染编译后的dom
+	console.log(Edom)
+}
+
